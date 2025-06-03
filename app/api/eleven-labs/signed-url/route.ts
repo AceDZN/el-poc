59e94864243400ef7843752501eaef60a7d15cd9
Alex Sindalovsky <design@acedzn.com>
2025-06-03 18:54:18 +0300

    Update route.ts

--- app/api/eleven-labs/signed-url/route.ts
+++ app/api/eleven-labs/signed-url/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 
 export async function GET() {
   const agentId = 'pIoxHtD84aviMmoAuWqq';
-  const apiKey = 'sk_a584a824c02087512bd4599dd81ae37f6b2a5decc2e3eb43';
+  const apiKey = process.env.ELEVENLABS_API_KEY;
 
   try {
     const response = await fetch(
